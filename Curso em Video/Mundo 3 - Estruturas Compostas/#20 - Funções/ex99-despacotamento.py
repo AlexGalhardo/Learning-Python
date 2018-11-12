dd1478c1a829b2b75e3ce0ba1cda7bb0e13e2339
galhardo <aleexgvieira@gmail.com>
2018-11-12 21:57:28 -0200

    up

--- Curso em Video/Mundo 3 - Estruturas Compostas/#20 - Funções/ex99-despacotamento.py
+++ Curso em Video/Mundo 3 - Estruturas Compostas/#20 - Funções/ex99-despacotamento.py
@@ -1,8 +1,11 @@
+from time import sleep
+
 def despacotamento(*numeros):
 	print('-='*20)
 	maior = -99999
 	for parametro in numeros:
 		print('{} '.format(parametro), end='')
+		sleep(0.5)
 		if(parametro > maior):
 			maior = parametro
 	print('\nO numero total de numeros informados foram: {}.'.format(len(numeros)))
